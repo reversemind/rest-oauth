50f5d312dfaacbc3e59cb1800f03ddde95d07d22
Eugene Kalinin <kalinin.eugene@gmail.com>
2014-04-16 22:36:49 +0400

    DB integration

--- src/main/java/ru/ttk/baloo/rest/services/JSONDTOSimpleController.java
+++ src/main/java/ru/ttk/baloo/rest/services/JSONDTOSimpleController.java
@@ -2,9 +2,13 @@ package ru.ttk.baloo.rest.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.ttk.baloo.rest.dto.HouseDTO;
+
+import java.security.Principal;
 
 /**
  *
@@ -18,7 +22,14 @@ public class JSONDTOSimpleController {
     @RequestMapping(value = "/house", method = RequestMethod.POST, consumes="application/json")
     public
     @ResponseBody
-    String createHouse(@RequestBody HouseDTO houseDTO) {
+    String createHouse(@RequestBody HouseDTO houseDTO, OAuth2Authentication auth) {
+
+        LOG.info("FFFFF:" + auth);
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        LOG.info("currentUser:" + principal);
+
         LOG.info("HouseDTO:" + houseDTO);
         return "{\"code\" : \"OK\"}";
 //        return new RestResponse(new Header(Header.CODE.OK, Header.MESSAGE.OK), new Payload());
